9a15e5894270047d6ff239767e592d4076c9179b
Taylor Radke <taylor.radke@outlook.com>
2019-05-06 19:49:41 +1000

    fixed up and down heap

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 import math
 
 class MaxHeap:
-    def __init__(self):
+    
+    def __init__(self):    
         self.root = None
         self.insertion_stack = []
 
@@ -12,15 +13,16 @@ class MaxHeap:
             self.root = self.Node(None, value)
             self.last_insertion = self.root
         else:
-            insertion = self.next_insertion()
+            parent = self.next_insertion()
 
-            if not insertion.left:
-                insertion.left = self.Node(insertion, value)
-                self.last_insertion = insertion.left
+            if not parent.left:
+                parent.left = self.Node(parent, value)
+                self.last_insertion = parent.left
             else:
-                insertion.right = self.Node(insertion, value)
-                self.last_insertion = insertion.right
-            self.upheap()
+                parent.right = self.Node(parent, value)
+                self.last_insertion = parent.right
+
+        self.upheap()
         self.insertion_stack.append(self.last_insertion)
     
     def next_insertion(self):
@@ -33,12 +35,10 @@ class MaxHeap:
                 return node
 
             if node.is_left_child():
-                if not node.is_right_child():
-                    return node.parent
-
-                node = node.parent.right
-                while node.left:
-                    node = node.left
+                if node.parent.right:
+                    node = node.parent.right
+                    while node.left:
+                        node = node.left
                 return node
             node = node.parent
 
@@ -49,12 +49,35 @@ class MaxHeap:
             node.swap(node.parent)
             node = node.parent
 
+    def downheap(self, node):
+        left = node.left
+        right = node.right
+
+        if left:
+            if right:
+                if left.value[0] > node.value[0] or right.value[0] > node.value[0]:
+                    if left.value[0] > right.value[0]:
+                      #  print(f"left, swapping {left.value[0]} with {node.value[0]}")
+                        node.swap(left)
+                        self.downheap(left)
+                    elif right.value[0] > left.value[0]:
+                      #  print(f"right, swapping {right.value[0]} with {node.value[0]}")
+                        node.swap(right)
+                        self.downheap(right)
+            else:
+                if left.value[0] > node.value[0]:
+                    #print(f"left, swapping {left.value[0]} with {node.value[0]}")
+                    node.swap(left)
+                    self.downheap(left)
+            
+
     def heapify(self, values):
         for val in values:
             self.insert(val)
 
     def remove_max(self):
         #Get current max value in root
+
         max_value = self.root.value
         
         if self.last_insertion is self.root:
@@ -67,18 +90,13 @@ class MaxHeap:
         self.insertion_stack.pop()
 
         # Set child of parent to None
-        if self.last_insertion.is_left_child():
-            self.last_insertion.parent.left = None
-        elif self.last_insertion.is_right_child():
-            self.last_insertion.parent.right = None
+        if self.last_insertion.is_left_child():     self.last_insertion.parent.left = None
+        elif self.last_insertion.is_right_child():  self.last_insertion.parent.right = None
 
         self.last_insertion = self.insertion_stack[-1]
 
-        self.downheap()
+        self.downheap(self.root)
         return max_value
-
-    def downheap(self):
-        pass
 
     #Max Heap node struct         
     class Node:
@@ -119,7 +137,6 @@ def sum_of_digits(val):
 
 
 if __name__ == "__main__":
-    
     with open(sys.argv[1]) as input_file:
         test_cases = int(input_file.readline().replace("\n", ""))
 
@@ -129,13 +146,16 @@ if __name__ == "__main__":
             balls = [(int(i),sum_of_digits(int(i))) for i in input_file.readline().replace("\n","").replace("\t", " ").split(" ")]
             toss_result = input_file.readline().replace("\n","").lower()
 
-            scott_heap = MaxHeap()
-            rusty_heap = PrioritySumDigits()
+            value_priority_player = MaxHeap()
+            sum_of_digits_priority_player = PrioritySumDigits()
 
-            scott_heap.heapify(balls)
-            rusty_heap.heapify(balls)
+            value_priority_player.heapify(balls)
 
-            for _ in range(len(balls)):
-                print(rusty_heap.remove_max())
-            
-            print("===========================")
+            for _ in balls:
+                print(value_priority_player.remove_max())
+
+
+
+            print("=" * 30)
+
+
