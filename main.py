aaba5c036433824224770f2ec5e691d5e82bef00
Taylor Radke <taylor.radke@outlook.com>
2019-05-09 19:15:01 +1000

    made next_insertion recursive

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 #!/usr/bin/python3.6
 import sys
 import math
+from time import time
+
 
 class MaxHeap:
     
-    def __init__(self):    
+    def __init__(self, name, init_values = None):    
         self.root = None
         self.insertion_stack = []
         self.score = 0
+        self.name = name
+
+        if init_values:
+            self.heapify(init_values)
+
+    def __str__(self):
+        return f"{self.name}: {self.root.value}"
 
     def insert(self, value):
-        if not self.root:
+        if self.root is None:
             self.root = self.Node(None, value)
             self.last_insertion = self.root
         else:
-            parent = self.next_insertion()
+            parent = self.next_insertion(self.last_insertion)
 
-            if not parent.left:
+            if parent.left is None:
                 parent.left = self.Node(parent, value)
                 self.last_insertion = parent.left
             else:
                 parent.right = self.Node(parent, value)
                 self.last_insertion = parent.right
 
-        self.upheap(self.last_insertion)
+            self.upheap(self.last_insertion)
+            
         self.insertion_stack.append(self.last_insertion)
-    
-    def next_insertion(self):
-        node = self.last_insertion
 
-        while True:
-            if node is self.root:
+    def next_insertion(self,node):
+        if node is self.root:
+            while node.left:
+                node = node.left
+            return node
+        
+        if node.is_left_child():
+            if node.parent.right:
+                node = node.parent.right
                 while node.left:
                     node = node.left
-                return node
-
-            if node.is_left_child():
-                if node.parent.right:
-                    node = node.parent.right
-                    while node.left:
-                        node = node.left
-                return node
-            node = node.parent
+            return node
+        
+        return self.next_insertion(node.parent)
 
     def upheap(self, node):
-        while node is not self.root and node > node.parent:
+        if node is not self.root and node > node.parent:
             node.swap(node.parent)
-            node = node.parent
+            self.upheap(node.parent)
 
     def downheap(self, node):
         left = node.left
@@ -68,24 +76,24 @@ class MaxHeap:
                 self.downheap(left)
 
     def heapify(self, values):
-        for val in values:
-            self.insert(val)
+        for value in values:
+            self.insert(value)
 
     def remove_max(self):
         #Get current max value in root
-
-        max_value = self.root.value
         
-        if not self.root.left and not self.root.right:
-            self.root = None
-            return max_value
+        max_value = self.root.value
 
+        if len(self.insertion_stack) == 1:
+            return max_value
+        
         #Swap root value with last inserted node
         self.root.swap(self.last_insertion)
         
         self.delete_last_inserted()
 
         self.downheap(self.root)
+       # self.sort(None,self.root)
         return max_value
     
     def delete_last_inserted(self):
@@ -96,12 +104,12 @@ class MaxHeap:
         if self.insertion_stack:
             self.last_insertion = self.insertion_stack[-1]
         else:
+            #Heap is empty
             self.root = None
 
     def remove_value(self,value):
         #Find first occurrence of value in heap
-        frontier = []
-        frontier.append(self.root)
+        frontier = [self.root]
 
         while frontier:
             node = frontier.pop()
@@ -140,6 +148,9 @@ class MaxHeap:
         def __gt__(self,other):
             return self.value[0] > other.value[0]
 
+        def __lt__(self,other):
+            return self.value[0] < other.value[0]
+
         def __eq__(self,other):
             return self.value[0] == other.value[0]
 
@@ -165,6 +176,7 @@ class MaxHeap:
 
 class PrioritySumDigits(MaxHeap):
     #Override how this class prioritises a nodes value
+
     class Node(MaxHeap.Node):
         def __eq__(self,other):
             return self.value[0] == other.value[0] and self.value[1] == other.value[1]
@@ -173,55 +185,56 @@ class PrioritySumDigits(MaxHeap):
             return self.value[0] != other.value[0] and self.value[1] != other.value[1]
 
         def __gt__(self,other):
-            if self.value[1] > other.value[1]:
-                return True
-            elif self.value[1] < other.value[1]:
-                return False
-            elif self.value[1] == other.value[1]:
+            if self.value[1] != other.value[1]:
+                return self.value[1] > other.value[1]
+            else:
                 return self.value[0] > other.value[0]
+            
+        def __lt__(self,other):
+            if self.value[1] != other.value[1]:
+                return self.value[1] < other.value[1]
+            else:
+                return self.value[0] < other.value[0]
 
 def sum_of_digits(val):
     sum = 0
     for n in range(0, int(math.log10(val)+1)):
-        sum += ((1 / pow(10,n) * ( val % pow(10,n + 1)  - (val % pow(10,n)))))
+        sum += ((1 / pow(10,n) * (val % pow(10,n + 1) - (val % pow(10,n)))))
     return int(sum)
 
 
-def main(input):
-    with open(sys.argv[1]) as input_file:
+def main(input_file):
+    with open(input_file) as input_file:
         with open("output.txt",'w') as output_file:
             test_cases = int(input_file.readline().replace("\n", ""))
 
             for i in range(0, test_cases):
-                n,k = input_file.readline().replace("\n","").split(" ")
-                    
+                n, k = input_file.readline().replace("\n","").split(" ")
+                n, k = int(n), int(k)
+
                 balls = [(int(i),sum_of_digits(int(i))) for i in input_file.readline().replace("\n","").replace("\t", " ").split(" ")]
                 toss_result = input_file.readline().replace("\n","").lower()
 
-                value_priority_player = MaxHeap()
-                sum_of_digits_priority_player = PrioritySumDigits()
-
-                value_priority_player.heapify(balls)
-                sum_of_digits_priority_player.heapify(balls)
-                
+                value_priority_player = MaxHeap("SCOTT", balls)
+                digit_sum_priority_player = PrioritySumDigits("RUSTY", balls)
+            
                 if toss_result == 'heads':
-                    current_player = value_priority_player
-                    waiting_player = sum_of_digits_priority_player
+                    current_player, waiting_player = value_priority_player, digit_sum_priority_player
                 elif toss_result == 'tails':
-                    current_player = sum_of_digits_priority_player
-                    waiting_player = value_priority_player
+                    current_player, waiting_player = digit_sum_priority_player, value_priority_player
 
-                while not current_player.is_empty():
-                    for _ in range(int(k)):
-                        if not current_player.is_empty():
-                            value, digit_sum = current_player.remove_max()
-                            current_player.score += value
-                            waiting_player.remove_value((value,digit_sum))
-                        else:
-                            break
-                    current_player, waiting_player = waiting_player, current_player
+                turns = 0
+                while turns < n:
+                    #  print(current_player)
+                    value, digit_sum = current_player.remove_max()
+                    current_player.score += value
+                    waiting_player.remove_value((value,digit_sum))
+                    turns+=1
 
-                output_file.write(f"{str(value_priority_player.score)} {str(sum_of_digits_priority_player.score)}\n")
+                    if turns % k == 0:
+                        current_player, waiting_player = waiting_player, current_player
+
+                output_file.write(f"{str(value_priority_player.score)} {str(digit_sum_priority_player.score)}\n")
 
 if __name__ == "__main__":
     main(sys.argv[1])
